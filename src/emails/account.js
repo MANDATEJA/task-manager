8aa1b3b91601ef9fdbe4a4b7e41a667139a1ae47
teja <tejareddy.teju@gmail.com>
2019-10-09 13:06:20 +0530

    Heroku ready project

--- src/emails/account.js
+++ src/emails/account.js
@@ -1,11 +1,33 @@
 const sgMail = require('@sendgrid/mail')
-const sendgridAPIKey = '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'
 
-sgMail.setApiKey(sendgridAPIKey)
+sgMail.setApiKey(process.env.SENDGRID_API_KEY)
 
-sgMail.send({
-    to: 'tejareddy.teju@gmail.com',
-    from: 'tejareddy.teju@gmail.com',
-    subject: 'First email from TaskApp',
-    text: 'Test email from TaskApp'
-})
+// sgMail.send({
+//     to: 'tejareddy.teju@gmail.com',
+//     from: 'tejareddy.teju@gmail.com',
+//     subject: 'Thanks for joining in!',
+//     text: `Welcome to the app. Let me know how you get along with the app.`
+// })
+
+const sendWelcomeEmail = (email, name) => {
+    sgMail.send({
+        to: email,
+        from: 'tejareddy.teju@gmail.com',
+        subject: 'Thanks for joining in!',
+        text: `Welcome to the app, ${name}. Let me know how you get along with the app.`
+    })
+}
+
+const sendGoodbyeEmail = (email, name) => {
+    sgMail.send({
+        to: email,
+        from: 'tejareddy.teju@gmail.com',
+        subject: 'Until next time!!',
+        text: `Goodbye ${name}. Was there anything that we could have done to keep your from cencelling your account? Please let us know.`
+    })
+}
+
+module.exports = {
+    sendWelcomeEmail,
+    sendGoodbyeEmail
+}
